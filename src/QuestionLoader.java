c504707b7099816c7987a510049aa49c1183d5b1
Hudas <duartejudeanthone@gmail.com>
2025-01-09 15:55:00 +0800

    90% DONE! error handling na lang at round option!

--- src/QuestionLoader.java
+++ src/QuestionLoader.java
@@ -6,18 +6,22 @@ public class QuestionLoader {
 
   // Array List method for loading question
   public static List<Question> loadQuestion(String difficulty) {
-    // array list to store Question objects 
+    
+    
+    // array list to store Question objects - BOOK REFFERENCE
     List<Question> questions = new ArrayList<>();
-    String filePath = "resources/" + difficulty + ".txt";
+    String filePath = "C:\\Users\\kript\\Downloads\\QUIZWARS-FINAL\\resources/" + difficulty + ".txt";
+    System.out.println("Looking for file at: " + filePath);
 
     
     try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+       
       String line;
       while ((line = reader.readLine()) != null) {
         String[] parts = line.split(",");
         if (parts.length == 6) {
           String questionText = parts[0];
-          String optionA = parts[1]; 
+          String optionA = parts[1];
           String optionB = parts[2];
           String optionC = parts[3];
           String optionD = parts[4];
@@ -30,9 +34,12 @@ public class QuestionLoader {
           }
         }
       }
-    } catch (IOException e) {
+    } catch (FileNotFoundException e) {
+      System.out.println("ERROR: File not found at " + filePath);
+    } catch (IOException e){
       e.printStackTrace();
     }
+
     return questions;
 
   }
